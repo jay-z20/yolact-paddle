ff85a1833b4e2f40dd4a77e9a2f970dc2c7908a1
Jay-z <2034263349@qq.com>
2021-08-12 20:01:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 python eval.py --trained_model yolact_base_54_800000.pdparams --output_coco_json  --dataset=coco2017_testdev_dataset --cuda=True
-python eval.py --trained_model yolact_im700_54_800000.pdparams --output_coco_json  --dataset=coco2017_testdev_dataset
+
+python eval.py --trained_model yolact_im700_54_800000.pdparams --output_coco_json  --dataset=coco2017_testdev_dataset  --cuda=True
 
 python train.py --config=yolact_base_config --batch_size=8 --trained_model=yolact_base_54_800000.pdparams
-
-python eval.py --trained_model yolact_base_54_800000.pdparams --output_coco_json --images=/home/aistudio/images/:/home/aistudio/res/
-"--images","./images/:./outputs/"
-python run_coco_eval.py
