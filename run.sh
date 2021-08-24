202b03470f1402741c644b446318979e5dbcec74
Jay-z <2034263349@qq.com>
2021-08-24 15:45:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,8 +9,12 @@ python train.py --config=yolact_resnet50_config --batch_size=8  --resume=./weigh
 # 预测 test-dev 
 python eval.py --trained_model yolact_resnet50_54_800000.dpparams --output_coco_json  --dataset=coco2017_testdev_dataset --cuda=True
 
+# 打包预测结果
+cd results
 
+cp mask_detections.json detections_test-dev2017_yolact_results.json
 
+zip detections_test-dev2017_yolact_results.zip detections_test-dev2017_yolact_results.json
 
 
 # python eval.py --trained_model yolact_base_54_800000.pdparams --output_coco_json  --dataset=coco2017_testdev_dataset --cuda=True
